2e492377161e3cbe07a078d4de6d1e723cadee41
Prapakorn Jaraworn <prapakorn.jw@gmail.com>
2018-05-01 15:44:36 +0700

    Add 2 example hello world and module

---
+++ 02_Module/app.js
@@ -0,0 +1,5 @@
+var custom = require('./custom_module.js');
+
+console.log('Value of pie :: %s',custom.pi);
+console.log('Value of pie :: %s',custom.getValueOfPie());
+console.log('Value of circular cylinder :: %s',custom.calculateValueOfCircularCylinder(5,5));
---
+++ 02_Module/custom_module.js
@@ -0,0 +1,12 @@
+var pi='3.14';
+
+var getValueOfPie = function(){
+    return pi;
+}
+
+var calculateValueOfCircularCylinder = function(redius,height){
+    return pi*(redius*height)
+}
+exports.pi=pi;
+exports.getValueOfPie=getValueOfPie;
+exports.calculateValueOfCircularCylinder=calculateValueOfCircularCylinder;
